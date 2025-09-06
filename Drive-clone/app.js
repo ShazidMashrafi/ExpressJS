1d5549a7c432bf69368b3fa3da9b1b5abf19963a
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-09-07 02:39:58 +0600

    implemented login system

--- Drive-clone/app.js
+++ Drive-clone/app.js
@@ -4,12 +4,14 @@ const dotenv = require('dotenv')
 dotenv.config()
 const connectDB = require('./config/db')
 connectDB()
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.use('/user', userRouter)
 
--- Drive-clone/routes/user.routes.js
+++ Drive-clone/routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const {body, validationResult} = require('express-validator') 
 const userModel = require('../models/user.model')
 
@@ -24,7 +25,7 @@ router.post('/register',
         const {username, email, password} = req.body
         const hashPass = await bcrypt.hash(password, 10) 
         
-        const newUser = await new userModel({
+        const newUser = await userModel.create({
             username,
             email,
             password: hashPass
@@ -33,6 +34,48 @@ router.post('/register',
         res.json(newUser)
     })
 
+router.get('/login', (req, res) => {
+    res.render('login')
+})
 
+router.post('/login', 
+    body('username').trim().isLength({min:3, max:30}),
+    body('password').trim().isLength({min:6}),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ 
+                errors: errors.array() 
+            })
+        }
+
+        const {username, password} = req.body
+        const user = await userModel.findOne({
+            username: username
+        })
+
+        if(!user) {
+            return res.status(400).json({
+                msg: 'username or password is incorrect' 
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        
+        if(!isMatch) {
+            return res.status(400).json({
+                msg: 'username or password is incorrect'
+            })
+        }
+
+        const token = jwt.sign({
+            userId : user._id,
+            email: user.email,
+            username: user.username
+        }, process.env.JWT_SECRET)
+
+        res.cookie('token', token)
+        res.send('Logged in')
+})
 
 module.exports = router;
