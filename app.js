359ba13fb626e194e3fca0e3074f50e8a8434204
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-09-06 16:46:28 +0600

    added form

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.set('view engine', 'ejs')
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.send('Home page')
 })
 
 app.get('/about', (req, res) => {
@@ -12,6 +14,15 @@ app.get('/about', (req, res) => {
 
 app.get('/profile', (req, res) => {
     res.send('Profile page')
+})
+
+app.get('/form', (req, res) => {
+    res.render('index')
+})
+
+app.get('/get-form-data', (req, res) => {
+    console.log(req.query)
+    res.send('Form data received')
 })
 
 app.listen(3000)
